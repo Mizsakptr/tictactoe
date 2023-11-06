bc46c803851a072c7d002e2f4e4f91464fe54938
Mizsakptr <mizsakptr@gmail.com>
2023-11-06 09:26:13 +0100

    Alapjáték befejezve
    Alapjáték befejezve

--- Tic-tac-toe/Assets/Scripts/Instantiating.cs
+++ Tic-tac-toe/Assets/Scripts/Instantiating.cs
@@ -5,31 +5,44 @@ using UnityEngine;
 public class Instantiating : MonoBehaviour
 {
     public GameObject cross;
-    public GameObject circle;
-    
+    public GameObject circle;    
     public GameObject Scripthandler;
-    public bool isPlayerOne;
 
-    private void Start()
+    public bool isPlayerOne;
+    public bool playerOneWon;
+    public bool playerTwoWon;
+    public bool isDraw;
+
+
+    void Start()
     {
         isPlayerOne = Scripthandler.GetComponent<GameState>().isPlayerOne;
     }
     
 
     void Update()
-    {       
+    {
 
-        if (Input.GetMouseButtonDown(0)) 
+        playerOneWon = Scripthandler.GetComponent<GameState>().playerOneWon;
+        playerTwoWon = Scripthandler.GetComponent<GameState>().playerTwoWon;
+        isDraw = Scripthandler.GetComponent<GameState>().isDraw;
+
+        if (Input.GetMouseButtonDown(0) && !playerOneWon && !playerTwoWon) 
         {
-
+           
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            RaycastHit hit;          
+            
 
             if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Playarea") && isPlayerOne)
             {
-               
-                Instantiate(cross, hit.collider.gameObject.transform.position, Quaternion.identity);
-                Debug.Log(isPlayerOne);              
+                Ray ray2 = new Ray(hit.transform.position, transform.up);
+                RaycastHit hit2;
+                if(!Physics.Raycast(ray2, out hit2))
+                {
+                    Instantiate(cross, hit.collider.gameObject.transform.position, Quaternion.identity);
+                    isPlayerOne = !isPlayerOne;
+                }                       
 
             }
             
@@ -37,13 +50,18 @@ public class Instantiating : MonoBehaviour
             if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Playarea") && !isPlayerOne)
             {
 
-                Instantiate(circle, hit.collider.gameObject.transform.position + new Vector3(0f, 0.5f, 0f), Quaternion.identity);
-                Debug.Log(isPlayerOne);                
+                Ray ray2 = new Ray(hit.transform.position, transform.up);
+                RaycastHit hit2;
+                if (!Physics.Raycast(ray2, out hit2))
+                {
+                    Instantiate(circle, hit.collider.gameObject.transform.position + new Vector3(0f, 0.5f, 0f), Quaternion.identity);
+                    isPlayerOne = !isPlayerOne;
+                }                
+                
+            }            
 
-            }
-
-            isPlayerOne = !isPlayerOne;
         }
-        
+
+      
     }
 }
